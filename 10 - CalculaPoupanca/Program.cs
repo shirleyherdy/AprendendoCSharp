0447deaf1e232e9fa32d4bd262c7fe7c92222945
Shirley <shirleyherdy@hotmail.com>
2021-03-19 13:22:32 -0300

    primeira aula de while

---
+++ 10 - CalculaPoupanca/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula poupança");
+
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
+                contadorMes ++;  // contadorMes += 1;  contadorMes = contadorMes + 1
+
+            }
+
+            /* // 0.36 %  0.0036
+            valorInvestido = valorInvestido + valorInvestido * 0.0036;
+            Console.WriteLine("Após 1 mês, você terá R$" + valorInvestido);
+
+            valorInvestido = valorInvestido + valorInvestido * 0.0036;
+            Console.WriteLine("Após 2 meses, você terá R$" + valorInvestido); */
+
+            Console.ReadLine();
+        }
+    }
+}
