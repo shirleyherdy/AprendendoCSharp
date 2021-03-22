7b4267757276aa4eaa20e4dc6f3ad8c3a66cc154
Shirley <shirleyherdy@hotmail.com>
2021-03-22 13:23:09 -0300

    aula for

--- 10 - CalculaPoupanca/Program.cs
+++ 10 - CalculaPoupanca/Program.cs
@@ -20,7 +20,9 @@ namespace _10___CalculaPoupanca
             {
                 valorInvestido = valorInvestido + valorInvestido * 0.0036;
                 Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
-                contadorMes ++;  // contadorMes += 1;  contadorMes = contadorMes + 1
+                // contadorMes =+ 1;
+                //contador<es = contadorMes + 1
+                contadorMes ++; 
 
             }
 
