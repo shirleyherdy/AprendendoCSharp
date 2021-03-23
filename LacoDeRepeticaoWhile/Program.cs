eb74cde510c3785ef083279a40d18efdc958781b
Shirley Herdy <shirleyherdy@hotmail.com>
2021-03-22 21:44:31 -0300

    laços de repetoção

---
+++ LacoDeRepeticaoWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacoDeRepeticaoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imprimindo números de 0 a 10 com While");
+
+            int contador = 0;
+            while (contador <= 10)
+            {
+                Console.WriteLine(contador);
+                contador++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P11 - CalculaPoupanca2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11___CalculaPoupanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Executando projeto 11 - Calcula poupança 2");
+
+
+            double valorInvestido = 1000;
+
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes ++)
+            //colocar a inicialização| condição booleana | incremento
+            {
+                valorInvestido *= 1.0036; // multiplicar por 1.0036 = valorInvestido = valorInvestido + valorInvestido * 0.0036
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
